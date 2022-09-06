778dfd0dd7ce75cbef92f7a4c7e43fd5a5d88384
wenjie <2609082088@qq.com>
2022-09-06 11:20:04 +0800

    feat-样式修改

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,21 @@
 const { defineConfig } = require('@vue/cli-service')
+const { resolve } = require('path')
+const cpns = resolve(__dirname, 'src/comconment')
+const view = resolve(__dirname, 'src/view')
 module.exports = defineConfig({
-  transpileDependencies: true
+  lintOnSave: false,
+  transpileDependencies: true,
+  configureWebpack: {
+    devServer: {
+      open: true,
+      port: 8888,
+      host: 'localhost'
+    },
+    resolve: {
+      alias: {
+        cpns,
+        view
+      }
+    }
+  }
 })
